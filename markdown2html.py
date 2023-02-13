d398e39e1b6c32618932225a214151e8372a24a0
Reginald Shawn Annan <shawnrannan@gmail.com>
2023-02-13 23:17:10 +0000

    fix task 0 final

--- markdown2html.py
+++ markdown2html.py
@@ -11,13 +11,13 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html')
+        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
         sys.exit(1)
 
     try:
         readme = open(sys.argv[1])
     except FileNotFoundError:
-        print(f'Missing {sys.argv[1]}')
+        print(f'Missing {sys.argv[1]}', file=sys.stderr)
         sys.exit(1)
 
     sys.exit(0)
